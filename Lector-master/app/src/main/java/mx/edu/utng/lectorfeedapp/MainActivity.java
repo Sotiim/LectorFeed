e6702ccbd10aee5ec0516df7dd4a1b3393cafbb8
Sotiim10 <56424278+Sotiim@users.noreply.github.com>
2019-12-13 16:48:32 -0600

    Add files via upload

---
+++ Lector-master/app/src/main/java/mx/edu/utng/lectorfeedapp/MainActivity.java
@@ -0,0 +1,41 @@
+package mx.edu.utng.lectorfeedapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+
+    ArrayList<String> rssLinks= new ArrayList<>();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button btnRediff = findViewById(R.id.btnRediff);
+        Button btnCinemaBlen = findViewById(R.id.btnCinemaBlend);
+        btnRediff.setOnClickListener(this);
+        btnCinemaBlen.setOnClickListener(this);
+
+        rssLinks.add("http://www.rediff.com/rss/moviesreviewsrss.xml");
+        rssLinks.add("http://www.cinemablend.com/rss_review.php");
+
+    }
+
+    public void onClick(View view){
+        switch (view.getId()){
+            case R.id.btnRediff:
+                startActivity(new Intent(MainActivity.this, RSSFeedActivity.class).putExtra("rssLink", rssLinks.get(0)));
+                break;
+            case R.id.btnCinemaBlend:
+                startActivity(new Intent(MainActivity.this, RSSFeedActivity.class).putExtra("rssLink", rssLinks.get(1)));
+                break;
+        }
+    }
+}
